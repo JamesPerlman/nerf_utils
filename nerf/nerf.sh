cad38829f209b97eca4007a2e3a46660876e4a15
JamesPerlman <jam.e.perl@gmail.com>
2022-08-20 19:42:32 -0700

    auto-activate conda when running nerf command

--- nerf/nerf.sh
+++ nerf/nerf.sh
@@ -2,6 +2,9 @@ THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 SUPPORTED_CMDS="prepare|train|render"
 
+eval "$(conda shell.bash hook)"
+conda activate $NGP_CONDA_ENV
+
 # thank you https://unix.stackexchange.com/a/111518
 if [[ "$1" =~ ^($SUPPORTED_CMDS)$ ]]; then
     python $THIS_DIR/$1.py "${@:2}"
@@ -9,3 +12,5 @@ else
     echo "Usage: nerf <cmd> <options>"
     echo "Where <cmd> is one of: $SUPPORTED_CMDS"
 fi
+
+conda deactivate

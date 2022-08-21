920cfcc4c1616c05f4e17691b7159b1d8bad6973
JamesPerlman <jam.e.perl@gmail.com>
2022-08-20 19:44:11 -0700

    better aliases

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@ THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 alias extract_frames="python $THIS_DIR/ffmpeg/extract_frames.py"
 
-alias ngp-process="$THIS_DIR/nerf/ngp-process.sh"
-
 alias nerf="$THIS_DIR/nerf/nerf.sh"
 
 export NGP_DIR="/c/Users/bizon/Developer/instant-ngp"
+export NGP_CONDA_ENV="instant-ngp"

cb5547cfe5b868c553bebd15ac62ece22a79ee79
JamesPerlman <jam.e.perl@gmail.com>
2022-08-25 18:43:04 -0700

    CONDA_CMDS + OTHER_CMDS

--- nerf/nerf.sh
+++ nerf/nerf.sh
@@ -1,16 +1,19 @@
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SUPPORTED_CMDS="prepare|train|render"
+CONDA_CMDS="prepare|train|render|reorient"
 
-eval "$(conda shell.bash hook)"
-conda activate $NGP_CONDA_ENV
+OTHER_CMDS="-"
 
 # thank you https://unix.stackexchange.com/a/111518
-if [[ "$1" =~ ^($SUPPORTED_CMDS)$ ]]; then
+if [[ "$1" =~ ^($CONDA_CMDS)$ ]]; then
+    eval "$(conda shell.bash hook)"
+    conda activate $NGP_CONDA_ENV
+    python $THIS_DIR/$1.py "${@:2}"
+    conda deactivate
+elif [[ "$1" =~ ^($OTHER_CMDS)$ ]]; then
     python $THIS_DIR/$1.py "${@:2}"
 else
     echo "Usage: nerf <cmd> <options>"
-    echo "Where <cmd> is one of: $SUPPORTED_CMDS"
+    echo "Where <cmd> is one of: $CONDA_CMDS|$OTHER_CMDS"
 fi
 
-conda deactivate

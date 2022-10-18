864092e9521eda8f86d0cf534e4e282ef1e7dcc3
JamesPerlman <jam.e.perl@gmail.com>
2022-10-17 20:59:03 -0700

    rework preview.py, allow using transforms or snapshot

--- nerf/project.py
+++ nerf/project.py
@@ -6,5 +6,5 @@ def get_project_snapshot_name(n_steps: int) -> str:
     return f"snapshot-{n_steps}.msgpack"
 
 def get_project_snapshot_path(project_root: Path, n_steps) -> Path:
-    return project_root / "training" / get_project_snapshot_name(n_steps)
+    return project_root / "ngp" / get_project_snapshot_name(n_steps)
     

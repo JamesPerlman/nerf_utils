c9ad6d7e05c1ad1081a5bd649db62f49b643f822
JamesPerlman <jam.e.perl@gmail.com>
2022-10-17 20:58:51 -0700

    rework preview.py, allow using transforms or snapshot

--- nerf/preview.py
+++ nerf/preview.py
@@ -1,11 +1,19 @@
+from argparse import ArgumentParser
 import os
 import sys
 from pathlib import Path
 
-if len(sys.argv) < 2:
-    print(f"Usage: nerf preview <project/directory/or/transforms.json>")
-    exit()
-
 NGP_DIR = os.environ['NGP_DIR']
-scene_path = Path(os.getcwd()) / sys.argv[1]
-os.system(f"python \"{NGP_DIR}/scripts/run.py\" --gui --mode nerf --scene \"{str(scene_path.as_posix())}\"")
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--transforms", type=str, help="NeRF-style transforms.json used to load a scene.")
+    parser.add_argument("--snapshot", type=str, help="Snapshot to load.")
+    args = parser.parse_args()
+
+    if args.transforms:
+        scene_path = Path(args.transforms)
+        os.system(f"python \"{NGP_DIR}/scripts/run.py\" --gui --mode nerf --scene \"{str(scene_path.as_posix())}\"")
+    elif args.snapshot:
+        snapshot_path = Path(args.snapshot)
+        os.system(f"python \"{NGP_DIR}/scripts/run.py\" --gui --mode nerf --load_snapshot \"{str(snapshot_path.as_posix())}\"")

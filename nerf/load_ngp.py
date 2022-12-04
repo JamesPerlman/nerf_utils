61093568e2bbe4033678bf4db7dbdeb943ea1797
JamesPerlman <jam.e.perl@gmail.com>
2022-12-04 12:39:45 -0800

    fix pyd_dirs

--- nerf/load_ngp.py
+++ nerf/load_ngp.py
@@ -7,8 +7,8 @@ NGP_DIR = os.environ['NGP_DIR']
 
 pyd_dirs = []
 
-pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build*", "**/*.pyd"), recursive=True)]
-pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build*", "**/*.so"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.pyd"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.so"), recursive=True)]
 pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.pyd"), recursive=True)]
 pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.so"), recursive=True)]
 

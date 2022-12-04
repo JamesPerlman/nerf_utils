6053b3a6db4775336815649238f1117697a012c2
JamesPerlman <jam.e.perl@gmail.com>
2022-12-04 11:44:05 -0800

    don't need dng2png

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-dng2png() {
-    find $1 -name "*.dng" | parallel -I% --max-args 1 dcraw -c -w % '|' pnmtopng '>' $2/{/.}.png
-}
-
 alias nerf="$THIS_DIR/nerf/nerf.sh"
 
 export NGP_DIR="/c/Users/bizon/Developer/blender-ngp"

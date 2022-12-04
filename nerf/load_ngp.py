52bd811e7b00271536edfcb4a404fe52c06a6bd8
JamesPerlman <jam.e.perl@gmail.com>
2022-12-03 23:39:30 -0800

    search for pyngp in more dirs

--- nerf/load_ngp.py
+++ nerf/load_ngp.py
@@ -5,5 +5,13 @@ import sys
 
 NGP_DIR = os.environ['NGP_DIR']
 
-sys.path += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.pyd"), recursive=True)]
-sys.path += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.so"), recursive=True)]
+pyd_dirs = []
+
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build*", "**/*.pyd"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build*", "**/*.so"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.pyd"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.so"), recursive=True)]
+
+for pyd_dir in pyd_dirs:
+    if not pyd_dir in sys.path:
+        sys.path.append(pyd_dir)
